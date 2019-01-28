c6f53d3d145b441a81605b6f5e6ae497f4148f1a
Michael F. Ellis <michael.f.ellis@gmail.com>
2019-01-28 17:52:06 -0500

    development snapshot

--- ftee.go
+++ ftee.go
@@ -1,4 +1,161 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+var removeoutputs bool = false
+var outputs = make(map[string]*os.File)
+
 func main() {
+	outputs := make(map[string]*os.File)
+	defer removeOutputFiles()
+	defer closeOutputFiles()
+
+	var delimiter string
+	flag.StringVar(&delimiter, "d", "FTEE", "the delimiter tag")
+	flag.Parse()
+	delimiter = "FTEE"
+	infiles := flag.Args()
+
+	for _, infname := range infiles {
+		// Open the input file
+		infd, err := os.Open(infname)
+		if err != nil {
+			removeoutputs = true
+			log.Fatalf("Couldn't open input file: %q", err)
+		}
+		outputs, err = processInputFile(infd, delimiter, outputs)
+		if err != nil {
+			removeoutputs = true
+			log.Fatalf("Error processing %s: %q", infname, err)
+		}
+	}
+}
+
+// extractFileNames parses a line of text. If the line doesn't contain the
+// delimiter, it returns an empty slice and a nil error to indicate that this
+// line is to be output to whatever file targets are currently in effect.
+// Otherwise it splits the line on whitespace. Each field after the delimiter
+// is presumed to to be a file name and is appended to the names slice. Non-nil
+// errors are returned unless the delimiter is found in exactly one field and
+// there is at least on field following it.
+func extractFileNames(delimiter string, line string) (names []string, err error) {
+	// Short circuit if line doesn't contain delimiter
+	if !strings.Contains(line, delimiter) {
+		return
+	}
+	fields := strings.Fields(line)
+	dfound := false
+	for _, field := range fields {
+		if !dfound {
+			if field == delimiter {
+				dfound = true
+			}
+			continue
+		}
+		if field == delimiter {
+			err = fmt.Errorf("Found more than one delimiter %s in line.", delimiter)
+			return names, err
+		}
+		names = append(names, field)
+	}
+	switch dfound {
+	case false:
+		err = fmt.Errorf("Delimiter %s must be surrounded by whitespace", delimiter)
+	case true:
+		if len(names) == 0 {
+			err = fmt.Errorf("No file names found after delimiter %s", delimiter)
+		}
+	}
+	return names, err
+}
+
+// openOutputFiles is called with results from extractFileNames. For each name
+// in the list, It checks the outputs map to see if the file is already opened.
+// If so, it ignores the name and moves on to the next one.  Otherwise it
+// attempts to open the file for writing, truncating it if it exists. If
+// successful it adds it to outputs map. On failure, it returns the error from
+// os.Create immediately without attempting to open any further files from the
+// names list.
+func openOutputFiles(names []string, outputs map[string]*os.File) (map[string]*os.File, error) {
+	var err error = nil
+	for _, name := range names {
+		isnew := true
+		for oname, _ := range outputs {
+			if oname == name {
+				isnew = false
+				break
+			}
+		}
+		if isnew {
+			fd, err := os.Create(name)
+			if err != nil {
+				return outputs, err
+			}
+			outputs[name] = fd
+		}
+	}
+	return outputs, err
+}
+
+// closeOutputFiles is used as a deferred call in main to ensure that all
+// output files are closed on exit.
+func closeOutputFiles() {
+	for _, fd := range outputs {
+		fd.Close()
+	}
+}
+
+// removeOutputFiles is used as a deferred call in main to ensure that all
+// output files are removed if an error has occurred.
+func removeOutputFiles() {
+	if removeoutputs {
+		for name, _ := range outputs {
+			os.Remove(name)
+		}
+	}
+}
+
+func processInputFile(fd *os.File, delimiter string, outputs map[string]*os.File) (map[string]*os.File, error) {
+	var err error = nil
+	reader := bufio.NewReader(fd)
+	var targets = make([]*os.File, 0)
+	var line string
+	for {
+		line, err = reader.ReadString('\n')
+		if err == io.EOF {
+			err = nil
+			break
+		}
+		if err != nil {
+			return outputs, err
+		}
+		names, err := extractFileNames(delimiter, line)
+		if err != nil {
+			return outputs, err
+		}
+		if len(names) == 0 {
+			// lineout := line + "\n"
+			for _, f := range targets {
+				f.WriteString(line)
+			}
+		} else {
+			outputs, err = openOutputFiles(names, outputs)
+			if err != nil {
+				return outputs, err
+			}
+			targets = make([]*os.File, 0)
+			for _, name := range names {
+				targets = append(targets, outputs[name])
+			}
+		}
+	}
+	return outputs, err
 }
--- ftee_test.go
+++ ftee_test.go
@@ -1,6 +1,69 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"testing"
+)
 
-func TestMatchDelimter(t *testing.T) {
+func TestExtractFileNames(t *testing.T) {
+	// Good lines
+	line := "// FTEE foo.bar baz.txt"
+	expected := []string{"foo.bar", "baz.txt"}
+	names, err := extractFileNames("FTEE", line)
+	if err != nil {
+		t.Errorf("Unexpected error \"%q\" parsing \"%s\"", err, line)
+	}
+	if !reflect.DeepEqual(names, expected) {
+		t.Errorf("Expected %s got %s", expected, names)
+	}
+	// Output lines (no FTEE)
+	line = "lorem ipsum sit amet ..."
+	names, err = extractFileNames("FTEE", line)
+	if err != nil {
+		t.Errorf("Unexpected error \"%q\" parsing \"%s\"", err, line)
+	}
+
+	// Bad lines
+	line = "//FTEE foo.bar baz.txt"
+	errexp := fmt.Errorf("Delimiter FTEE must be surrounded by whitespace")
+	names, err = extractFileNames("FTEE", line)
+	if !reflect.DeepEqual(err, errexp) {
+		t.Errorf("Expected %q got %q", errexp, err)
+	}
+	line = "// FTEE foo.bar FTEE baz.txt"
+	errexp = fmt.Errorf("Found more than one delimiter FTEE in line.")
+	names, err = extractFileNames("FTEE", line)
+	if !reflect.DeepEqual(err, errexp) {
+		t.Errorf("Expected %q got %q", errexp, err)
+	}
+	line = "// FTEE"
+	errexp = fmt.Errorf("No file names found after delimiter FTEE")
+	names, err = extractFileNames("FTEE", line)
+	if !reflect.DeepEqual(err, errexp) {
+		t.Errorf("Expected %q got %q", errexp, err)
+	}
+
+}
+
+func TestOpenOutputFiles(t *testing.T) {
+	outputs := make(map[string]*os.File)
+	names := []string{"/tmp/foo.txt", "/tmp/bar.txt"}
+	updated, err := openOutputFiles(names, outputs)
+	if err != nil {
+		t.Errorf("Unexpected error: %q", err)
+	}
+	if len(updated) != 2 {
+		t.Errorf("Expected 2 opened files, got %d", len(updated))
+	}
+	updated2, err := openOutputFiles(names, updated)
+	if err != nil {
+		t.Errorf("Unexpected error: %q", err)
+	}
+	if !reflect.DeepEqual(updated, updated2) {
+		t.Errorf("Expected %v got %v", updated, updated2)
+	}
+	closeOutputFiles()
+	removeOutputFiles()
 }
